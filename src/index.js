83c8c1888ff967d02b23ba677f21a9cc5a2603f8
Tiffany <tiffany@Tiffanys-MacBook-Pro.local>
2026-07-21 09:53:39 +0100

    fix: use toast provider to render app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/global.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/global.css";
+import App from "./App";
+import { ToastProvider } from "./components/common/ToastContainer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ToastProvider>
     <App />
-  </React.StrictMode>
+  </ToastProvider>,
 );
